c4a8399bd874f2a51cc58084895e8b346ae0ef9b
Emile <emile.schockaert@student.kuleuven.be>
2023-11-29 10:38:45 +0100

    help function euclidean distance

---
+++ euclidean_distance.m
@@ -0,0 +1,13 @@
+function [distance] = euclidean_distance(position1,position2)
+%UNTITLED Summary of this function goes here
+%   Calculate the Euclidean distance between 2 points in space
+%   Postion1 and Position2 should have same length
+distance = 0;
+dimensions = length(position1);
+
+for i=1:dimensions
+    distance = (position1(1) - position2(1))^2 + (position1(2) - position2(2))^2 + (position1(3) - position2(3))^2;
+end
+
+end
+
