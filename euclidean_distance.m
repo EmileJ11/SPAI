1ca1def67519eb2c61d5ca94a25eb353aae0686f
Emile <emile.schockaert@student.kuleuven.be>
2023-11-29 15:07:03 +0100

    square root over distance

--- euclidean_distance.m
+++ euclidean_distance.m
@@ -6,7 +6,7 @@ distance = 0;
 dimensions = length(position1);
 
 for i=1:dimensions
-    distance = (position1(1) - position2(1))^2 + (position1(2) - position2(2))^2 + (position1(3) - position2(3))^2;
+    distance = sqrt((position1(1) - position2(1))^2 + (position1(2) - position2(2))^2 + (position1(3) - position2(3))^2);
 end
 
 end
