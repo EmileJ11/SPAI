5c39ee34590723baa20ff411bd5787cc17cfcd2f
Emile <emile.schockaert@student.kuleuven.be>
2023-11-30 10:23:11 +0100

    exported (S)RIR files

--- truncate_RIR.m
+++ truncate_RIR.m
@@ -1,4 +1,4 @@
-function truncatedSignal = truncate_RIR(signal, threshold)
+function truncatedSignal = truncate_RIR(signal, threshold, start_offset)
     % Find the index of the first sample above the threshold
     startIndex = find(abs(signal) > threshold, 1, 'first');
 
@@ -8,5 +8,5 @@ function truncatedSignal = truncate_RIR(signal, threshold)
     end
 
     % Truncate the signal from the startIndex
-    truncatedSignal = signal(startIndex-500:end);
+    truncatedSignal = signal(startIndex-start_offset:end);
 end
