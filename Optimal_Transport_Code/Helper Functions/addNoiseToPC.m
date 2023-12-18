97d32a11d208182c51afcf1e17e0e058c39781e7
DESKTOP-2PQHMR0\Emile Peeters <emilej.peeters@gmail.com>
2023-12-18 20:47:11 +0100

    Removed Aaron code

--- Optimal_Transport_Code/Helper Functions/addNoiseToPC.m
+++
@@ -1,9 +0,0 @@
-function PCnoisy = addNoiseToPC(PCclean, posNoise, magNoise)
-% Created by: Aaron Geldert
-% Last modified: 17 Oct 2022
-
-PCnoisy = PCclean;
-PCnoisy.pos = PCclean.pos + posNoise*randn(size(PCclean.pos));
-PCnoisy.mass = PCclean.mass .* (1+ magNoise*randn(size(PCclean.mass)));
-
-end
--- Optimal_Transport_Code/Helper Functions/hammerAidhofProjection.m
+++
@@ -1,8 +0,0 @@
-function [xy] = hammerAidhofProjection(aziEle)
-    % Hammer-Aidhof projection
-    azi = aziEle(:, 1);
-    ele = aziEle(:, 2);
-    hap = @(azi, ele) [-cos(ele).*sin(azi/2) 0.5 * sin(ele)] ./ ...
-        (sqrt(1+cos(ele).*cos(azi/2)));
-    xy = hap (mod (azi+ pi, 2 * pi) - pi, ele);
-end
